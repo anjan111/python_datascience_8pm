4fb799ce2a3a6bc3cff9a4784f49819fb524857d
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-08-31 08:17:24 -0700

    Add files via upload

---
+++ 001_scriptmode.py
@@ -0,0 +1,13 @@
+# script mode  / batch mode ---> resualibity
+'''
+---> new file
+---> write the python statements
+---> save as .py
+---> run it
+---> out put ---> python shell
+'''
+
+a = 10
+b = 20
+print a+b
+print a*b
---
+++ 002_python_syntax.py
@@ -0,0 +1,174 @@
+###############################################################
+#################### Python Basic Syntax ######################
+###############################################################
+
+---> Syntax
+---> Program
+---> Instruction
+        ---> Comments and Statements
+---> Python Keywords
+---> Python Identifiers
+---> Python Variables
+---> Python Datatypes
+
+
+
+###############################################################
+''' Syntax '''
+---> The structure we should follow while  writing a program
+
+'''program'''
+---> The set of instuctions to do any one specific task
+
+'''Instruction'''
+---> A line of code with in a program
+---> There are 2 types of instructions
+    1. comment
+    2. statement
+
+'''comment'''
+---> An instruction which can't be executable
+---> Which are used to give the brief explanation each and every statement
+---> we can implimented in 2 ways
+    1. single line comment  (  # )
+    2. Multiline comment ('''  ''')
+
+''' statement '''
+--> An instructions which are executable by cpu
+--> Statements are used for to do a task
+
+#################################################################
+Python Keywords
+
+'''
+ ---> The names are which are already defined in python
+ ---> Each and every keyword its having it own specific task
+ ---> python2 ---> 31
+ ---> python3 ---> 35
+'''
+
+###################################################################
+Python Identifiers
+
+'''
+---> which are progrmmer / user defined words
+---> To give the name entitiies for variables, functions, classes
+'''
+Rules
+
+'''
+---> we can use alphabets  [ a - z ]/  [ A - Z ]
+---> we can use digits  [0 -9 ]
+---> we can use underscore   _
+---> don't use digits as a letter
+---> don't use  keyword
+---> don't use special character  [ ,.@#@$#$^#%&*(*)
+
+'''
+##################################################################
+Python variable
+'''
+---> Which are names
+---> which are used for to store the data in memory location
+---> we don't use declaration before using it
+---> we can directly store data to names 
+'''
+###########################################################3
+                    Python Datatypes
+############################################################
+    '''
+            ---> In python all datatypes are defined class
+            ---> Datatypes are used for to specify the what type of data
+            holded by variable
+            ---> In case of  python PL , The daata which is already in the
+            memory loc
+            ---> Based on your data , the memory location and size is allocated
+            to variabole
+
+
+            ---> there are 2 types
+
+            1. fundamental datatype
+            -------------------------
+                ---> A single variable can hold single data
+
+                    int ----> a = 12
+                    float---> a = 12.44534
+                    complex -> a = 12 + 34j
+                    bool    --> a = True  / False
+                    None   ----> a = None
+
+          
+            
+            2. Sequential Datatype :-
+            -------------------------
+
+            ---> A single  variable can hold the multiple data elements
+
+            str ---> a = 'soc' / b = "soc" / c = '''soc''', d= """soc"""
+            list --> a = [ 12,5.54, True]
+            tuple --> a = (12,4.4545,False)
+            set  ---> a = {12,343,4444}
+            dict ---> a = {12:"soc" , 45:"aof"}
+
+            
+            
+    '''
+
+a = 'soc' 
+b = '''soc
+kfgjkfl
+dkgjkfjg
+'''
+a = 'i don't know'
+b = "i don't know"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
